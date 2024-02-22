6ffe9901bf996fe66a6bb3ff50803f200129412b
Natalia Grishina <agrishinstudio@yandex.ru>
2024-02-22 14:40:47 +0300

    JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(initialCards, deleteCard) {
+  initialCards.forEach(function(item) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__title').textContent = item.name;
+
+    // @todo: Вывести карточки на страницу
+    placesList.append(cardElement);
+  });
+}
+createCard(initialCards, deleteCard);
 
 // @todo: Функция удаления карточки
+placesList.addEventListener('click', deleteCard);
 
-// @todo: Вывести карточки на страницу
+function deleteCard(event) {
+  if (event.target.className === 'card__delete-button') {
+    const parentNode = event.target.closest('.card');
+    parentNode.remove();
+  }
+};
