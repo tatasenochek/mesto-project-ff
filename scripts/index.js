b1cb6c8631e6e9ad1ff7167229ae786678855d95
Natalia Grishina <agrishinstudio@yandex.ru>
2024-02-22 17:48:23 +0300

    правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,35 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(initialCards, deleteCard) {
-  initialCards.forEach(function(item) {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+function createCard(item, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    cardElement.querySelector('.card__image').src = item.link;
-    cardElement.querySelector('.card__title').textContent = item.name;
+  // наполнение карточки контентом
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').alt = item.name;
+  cardElement.querySelector('.card__title').textContent = item.name;
 
-    // @todo: Вывести карточки на страницу
-    placesList.append(cardElement);
-  });
+  // @todo: Вывести карточки на страницу
+  placesList.append(cardElement);
+
+  // Обработчик удаления карточки
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  return cardElement;
 }
-createCard(initialCards, deleteCard);
 
 // @todo: Функция удаления карточки
-placesList.addEventListener('click', deleteCard);
-
 function deleteCard(event) {
   if (event.target.className === 'card__delete-button') {
     const parentNode = event.target.closest('.card');
     parentNode.remove();
   }
 };
+
+// Создание карточки и добавление в список
+initialCards.forEach(function(item) {
+  placesList.append(createCard(item));
+});
+
+// Обработчик на список карточек
+placesList.addEventListener('click', deleteCard);
