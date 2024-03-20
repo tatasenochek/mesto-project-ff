65d9ab4a6951a65f64c1d033d41bfd72f06b65dc
Natalia Grishina <agrishinstudio@yandex.ru>
2024-03-20 10:33:11 +0300

    перемещение папок в src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '../pages/index.css';
+import './pages/index.css';
 import { initialCards } from './components/cards.js';
 import { createCard, deleteCard, toggleLikeStatus } from './components/card.js';
 import { openModal, closeModal, closeModalHandler, closeModalButton } from './components/modal.js';
